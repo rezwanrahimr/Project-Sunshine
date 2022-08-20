26f6be3dde4e6373e3b9631d050ea5e7e598b643
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-20 17:48:15 +0600

    login added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
+     
+     <Routes>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
+     </Routes>
     </div>
   );
 }
